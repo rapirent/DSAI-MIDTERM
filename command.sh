e68f25c16ea9018a058770302f5243deea64bcc4
rapirent <rapirent@gmail.com>
2018-05-11 01:43:48 +0800

    add check.log to ignore file

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python main.py $(date +'--day %d --month %m') >> check.log
-python api_submit.py $(date +'--day %d --month %m') >> check.log
+python3 main.py $(date +'--day %d --month %m') >> check.log
+python3 api_submit.py $(date +'--day %d --month %m') >> check.log
