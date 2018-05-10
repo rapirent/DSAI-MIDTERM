8cff2c931059a39def688158aba95e74bc769b8a
rapirent <rapirent@gmail.com>
2018-05-11 01:49:45 +0800

    add the last year submit command

--- command.sh
+++ command.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /usr/bin/python3 main.py $(date +'--day %d --month %m') >> check.log
 /usr/bin/python3 api_submit.py $(date +'--day %d --month %m') >> check.log
+/usr/bin/python3 lastyear_submit.py >> check.log
