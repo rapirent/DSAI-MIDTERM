bbb00d835a0cb69e31600703582123d14d9145ae
rapirent <rapirent@gmail.com>
2018-05-11 02:09:30 +0800

    consider about the home directory

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/bin/python3 main.py $(date +'--day %d --month %m') >> check.log
-/usr/bin/python3 api_submit.py $(date +'--day %d --month %m') >> check.log
-/usr/bin/python3 lastyear_submit.py >> check.log
+/usr/bin/python3 ~/DSAI-MIDTERM/main.py $(date +'--day %d --month %m') >> check.log
+/usr/bin/python3 ~/DSAI-MIDTERM/api_submit.py $(date +'--day %d --month %m') >> check.log
+/usr/bin/python3 ~/DSAI-MIDTERM/lastyear_submit.py >> check.log
