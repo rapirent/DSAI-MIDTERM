3c87a611c10bc284bc6fca7377592699f640c4c8
rapirent <rapirent@gmail.com>
2018-05-11 01:47:42 +0800

    fix the python3 execute path

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 main.py $(date +'--day %d --month %m') >> check.log
-python3 api_submit.py $(date +'--day %d --month %m') >> check.log
+/usr/bin/python3 main.py $(date +'--day %d --month %m') >> check.log
+/usr/bin/python3 api_submit.py $(date +'--day %d --month %m') >> check.log
